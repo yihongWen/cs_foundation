895d559ceb0d36a417b7fbe3fc5442b2d1e271a4
wenyihong <787252975wyh@gmail.com>
2022-06-20 17:55:04 +0800

    实验2

---
+++ ch02_sy01.asm
@@ -0,0 +1,28 @@
+;实验2
+assume cs:code
+code segment
+         mov ax,0ffffh
+         mov ds,ax
+        ; 单步执行，ss\sp指令是一起执行的
+        ; 同时栈种会保存一些cs\ip寄存器的信息
+         mov ax,2200h
+         mov ss,ax
+         mov sp,0100h
+
+         mov ax,ds:[0]
+         add ax,ds:[2]
+         mov bx,ds:[4]
+         add bx,ds:[6]
+
+         push ax
+         push bx
+         pop ax
+         pop bx
+
+         push ds:[4]
+         push ds:[6]
+         
+         mov ax,4c00h
+         int 21h
+code ends
+end
